90e6e7b117de70e14038b68f92b879a58e4ef20d
Prateek Bisht <105870180+PratiikBisht@users.noreply.github.com>
2024-03-01 20:13:25 +0530

    FIRST.cpp

---
+++ CPP/FIRST.cpp
@@ -0,0 +1,20 @@
+#include "rosbot_control/rosbot_class.h"
+#include <ros/ros.h>
+
+using namespace std;
+int main(int argc, char**argv){
+    ros::init(argc, argv, "rosbot__node")
+    
+    RosbotClass rosbot;
+    rosbot.move();
+    float x_1= rosbot.get_position(1);
+    float y_1= rosbot.get_position(2);
+
+  ROS_INFO_STREAM(x_1<< "and"<< y_1);
+
+    float x_2 = rosbot.get_position(1);
+    float y_2= rosbot.get_position(2);
+    ROS_INFO_STREAM(x_2<<"and"<<y_2);
+  return 0; 
+
+}
